fc3d2dae735fa69e1a9c7612df08f6d51c53b2e6
Alexey Voznyuk <me@swizard.info>
2015-10-26 02:55:00 +0300

    getops related code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate getopts;
+
+use std::{io, env, process};
+use std::io::Write;
+use getopts::Options;
+
+#[derive(Debug)]
+enum Error {
+    Getopts(getopts::Fail),
+}
+
+fn entrypoint(maybe_matches: getopts::Result) -> Result<(), Error> {
+    let matches = try!(maybe_matches.map_err(|e| Error::Getopts(e)));
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let cmd_proc = args.next().unwrap();
+    let mut opts = Options::new();
+
+    opts.optopt("d", "database", "database directory path (optional, default: ./spiderq)", "");
+    opts.optopt("z", "zmq-addr", "zeromq interface listen address (optional, default: ipc://./spiderq.ipc)", "");
+
+    match entrypoint(opts.parse(args)) {
+        Ok(()) => (),
+        Err(cause) => {
+            let _ = writeln!(&mut io::stderr(), "Error: {:?}", cause);
+            let usage = format!("Usage: {}", cmd_proc);
+            let _ = writeln!(&mut io::stderr(), "{}", opts.usage(&usage[..]));
+            process::exit(1);
+        }
+    }
 }
