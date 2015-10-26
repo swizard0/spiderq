db84e33d321f999b502c37053f8bfddeb8b9c6d2
Alexey Voznyuk <me@swizard.info>
2015-10-26 15:42:19 +0300

    initial tests committed

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn entrypoint(maybe_matches: getopts::Result) -> Result<(), Error> {
     let matches = try!(maybe_matches.map_err(|e| Error::Getopts(e)));
     let database_dir = matches.opt_str("database").unwrap_or("./spiderq".to_owned());
 
-    let _db = try!(db::Database::new(database_dir).map_err(|e| Error::Db(e)));
+    let _db = try!(db::Database::new(&database_dir).map_err(|e| Error::Db(e)));
 
     Ok(())
 }
@@ -42,3 +42,69 @@ fn main() {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use super::db;
+    use super::db::Database;
+
+    fn mkdb(path: &str) -> Database {
+        let _ = fs::remove_dir_all(path);
+        Database::new(path).unwrap()
+    }
+
+    #[test]
+    fn make_database() {
+        let db = mkdb("/tmp/spiderq_a");
+        assert_eq!(db.count().unwrap(), 0);
+    }
+
+    #[test]
+    fn reopen_database() {
+        {
+            let db = mkdb("/tmp/spiderq_b");
+            assert_eq!(db.count().unwrap(), 0);
+        }
+        {
+            let db = Database::new("/tmp/spiderq_b").unwrap();
+            assert_eq!(db.count().unwrap(), 0);
+        }
+    }
+
+    #[test]
+    fn open_database_fail() {
+        match Database::new("/qwe") {
+            Ok(..) => panic!("expected fail"),
+            Err(..) => (),
+        }
+    }
+
+    fn mkfill(path: &str) -> Database {
+        let mut db = mkdb(path);
+        assert_eq!(db.add(&[1, 2, 3]).unwrap(), 0);
+        assert_eq!(db.add(&[4, 5, 6, 7]).unwrap(), 1);
+        assert_eq!(db.add(&[8, 9]).unwrap(), 2);
+        assert_eq!(db.count().unwrap(), 3);
+        db
+    }
+
+    #[test]
+    fn open_database_fill() {
+        let _ = mkfill("/tmp/spiderq_c");
+    }
+
+    #[test]
+    fn open_database_check() {
+        let mut db = mkfill("/tmp/spiderq_d");
+        let mut data = Vec::new();
+        assert_eq!(db.load(0, &mut data).unwrap(), &[1, 2, 3]);
+        assert_eq!(db.load(1, &mut data).unwrap(), &[4, 5, 6, 7]);
+        assert_eq!(db.load(2, &mut data).unwrap(), &[8, 9]);
+        match db.load(3, &mut data) {
+            Err(db::Error::IndexIsTooBig { given: 3, total: 3, }) => (),
+            other => panic!("unexpected Database::load return value: {:?}", other),
+        }
+    }
+}
+
