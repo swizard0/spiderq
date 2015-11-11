5a77efdbd64023cbbb3f72e45d4534a0edacf630
Alexey Voznyuk <me@swizard.info>
2015-11-12 01:58:08 +0300

    pq mod refactoring + tests

--- src/pq.rs
+++ src/pq.rs
@@ -1,12 +1,12 @@
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
 use time::SteadyTime;
-use super::proto::RepayStatus;
+use super::proto::{Key, RepayStatus};
 
-#[derive(PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 struct PQueueEntry {
+    key: Key,
     priority: u64,
-    index: u32,
     boost: u32,
 }
 
@@ -25,8 +25,8 @@ impl PartialOrd for PQueueEntry {
 #[derive(PartialEq, Eq)]
 struct LentEntry {
     trigger_at: SteadyTime,
+    key: Key,
     snapshot: u64,
-    index: u32,
 }
 
 impl Ord for LentEntry {
@@ -44,15 +44,15 @@ impl PartialOrd for LentEntry {
 pub struct PQueue {
     serial: u64,
     queue: BinaryHeap<PQueueEntry>,
-    lentm: HashMap<u32, (u64, PQueueEntry)>,
+    lentm: HashMap<Key, (u64, PQueueEntry)>,
     lentq: BinaryHeap<LentEntry>,
 }
 
 impl PQueue {
-    pub fn new(count: usize) -> PQueue {
+    pub fn new() -> PQueue {
         PQueue {
-            serial: count as u64,
-            queue: (0 .. count).map(|i| PQueueEntry { priority: i as u64, index: i as u32, boost: 0, }).collect(),
+            serial: 1,
+            queue: BinaryHeap::new(),
             lentm: HashMap::new(),
             lentq: BinaryHeap::new(),
         }
@@ -62,59 +62,57 @@ impl PQueue {
         self.queue.len()
     }
 
-    pub fn add(&mut self) -> u32 {
+    pub fn add(&mut self, key: Key) {
         self.serial += 1;
-        let last_index = self.len() as u32;
-        self.queue.push(PQueueEntry { priority: self.serial, index: last_index, boost: 0, });
-        last_index
+        self.queue.push(PQueueEntry { key: key, priority: self.serial, boost: 0, });
     }
 
-    pub fn top(&self) -> Option<u32> {
-        self.queue.peek().map(|e| e.index)
+    pub fn top(&self) -> Option<Key> {
+        self.queue.peek().map(|e| e.key.clone())
     }
 
-    pub fn lend(&mut self, trigger_at: SteadyTime) -> Option<u32> {
+    pub fn lend(&mut self, trigger_at: SteadyTime)  {
         if let Some(entry) = self.queue.pop() {
-            let index = entry.index;
-            self.lentm.insert(index, (self.serial, entry));
-            self.lentq.push(LentEntry { trigger_at: trigger_at, snapshot: self.serial, index: index, });
-            Some(index)
-        } else {
-            None
+            self.lentq.push(LentEntry { trigger_at: trigger_at, key: entry.key.clone(), snapshot: self.serial, });
+            self.lentm.insert(entry.key.clone(), (self.serial, entry));
         }
     }
 
     pub fn next_timeout(&mut self) -> Option<SteadyTime> {
-        while let Some(&LentEntry { trigger_at: trigger, snapshot: qshot, index: i, .. }) = self.lentq.peek() {
-            match self.lentm.get(&i) {
-                Some(&(mshot, _)) if mshot == qshot => 
-                    return Some(trigger),
-                _ => 
-                    self.lentq.pop(),
-            };
+        loop {
+            if let Some(&LentEntry { trigger_at: trigger, key: ref k, snapshot: qshot, .. }) = self.lentq.peek() {
+                match self.lentm.get(k) {
+                    Some(&(mshot, _)) if mshot == qshot => return Some(trigger),
+                    _ => (),
+                }
+            } else {
+                break
+            }
+
+            self.lentq.pop();
         }
         None
     }
     
     pub fn repay_timed_out(&mut self) {
-        if let Some(LentEntry { index: qi, .. }) = self.lentq.pop() {
-            self.repay(qi, RepayStatus::Front)
+        if let Some(LentEntry { key: k, .. }) = self.lentq.pop() {
+            self.repay(k, RepayStatus::Front)
         }
     }
 
-    pub fn repay(&mut self, index: u32, status: RepayStatus) {
-        if let Some((_, mut entry)) = self.lentm.remove(&index) {
-            self.serial += 1;
+    pub fn repay(&mut self, key: Key, status: RepayStatus) {
+        if let Some((_, mut entry)) = self.lentm.remove(&key) {
             let min_priority = if let Some(&PQueueEntry { priority: p, .. }) = self.queue.peek() { p } else { 0 };
-            let region = self.serial - min_priority;
+            let region = self.serial + 1 - min_priority;
             let current_boost = match status {
                 RepayStatus::Penalty if entry.boost == 0 => 0,
                 RepayStatus::Penalty => { entry.boost -= 1; entry.boost },
                 RepayStatus::Reward if region >> (entry.boost + 1) == 0 => entry.boost,
                 RepayStatus::Reward => { entry.boost += 1; entry.boost },
                 RepayStatus::Front => 0,
-                RepayStatus::Drop => unimplemented!(),
+                RepayStatus::Drop => return,
             };
+            self.serial += 1;
             entry.priority = match status {
                 RepayStatus::Front => 0,
                 _ => self.serial - (region - (region >> current_boost)),
@@ -126,59 +124,79 @@ impl PQueue {
 
 #[cfg(test)]
 mod test {
+    use std::sync::Arc;
     use time::{SteadyTime, Duration};
     use super::PQueue;
-    use super::super::proto::RepayStatus;
+    use super::super::proto::{Key, RepayStatus};
+
+    fn as_key(value: usize) -> Key {
+        Arc::new(format!("{}", value).into_bytes())
+    }
+
+    fn make_pq(len: usize) -> PQueue {
+        let mut pq = PQueue::new();
+        for i in 0 .. len {
+            pq.add(as_key(i));
+        }
+        pq
+    }
 
     #[test]
     fn basic_api() {
         let time = SteadyTime::now();
-        let mut pq = PQueue::new(10);
-        assert_eq!(pq.top(), Some(0));
-        assert_eq!(pq.lend(time + Duration::seconds(10)), Some(0));
-        assert_eq!(pq.top(), Some(1));
+        let mut pq = make_pq(10); // 0 1 2 3 4 5 6 7 8 9 |
+        assert_eq!(pq.top(), Some(as_key(0)));
+        pq.lend(time + Duration::seconds(10)); // 1 2 3 4 5 6 7 8 9 | 0
+        assert_eq!(pq.top(), Some(as_key(1)));
         assert_eq!(pq.next_timeout(), Some(time + Duration::seconds(10)));
-        assert_eq!(pq.lend(time + Duration::seconds(5)), Some(1));
-        assert_eq!(pq.top(), Some(2));
+        pq.lend(time + Duration::seconds(5)); // 2 3 4 5 6 7 8 9 | <1/5> <0/10>
+        assert_eq!(pq.top(), Some(as_key(2)));
         assert_eq!(pq.next_timeout(), Some(time + Duration::seconds(5)));
-        pq.repay(1, RepayStatus::Reward);
-        assert_eq!(pq.top(), Some(2));
+        pq.repay(as_key(1), RepayStatus::Reward); // 2 3 4 5 (1 6) 7 8 9 | <0/10>
+        assert_eq!(pq.top(), Some(as_key(2)));
         assert_eq!(pq.next_timeout(), Some(time + Duration::seconds(10)));
-        pq.repay_timed_out();
+        pq.repay_timed_out(); // 0 2 3 4 5 (1 6) 7 8 9 |
         assert_eq!(pq.next_timeout(), None);
-        assert_eq!(pq.lend(time + Duration::seconds(5)), Some(0));
-        assert_eq!(pq.lend(time + Duration::seconds(6)), Some(2));
-        assert_eq!(pq.lend(time + Duration::seconds(7)), Some(3));
-        assert_eq!(pq.lend(time + Duration::seconds(8)), Some(4));
-        assert_eq!(pq.lend(time + Duration::seconds(9)), Some(5));
-        assert_eq!(pq.lend(time + Duration::seconds(10)), Some(6));
-        assert_eq!(pq.lend(time + Duration::seconds(11)), Some(1));
-        assert_eq!(pq.lend(time + Duration::seconds(12)), Some(7));
-        assert_eq!(pq.lend(time + Duration::seconds(13)), Some(8));
-        assert_eq!(pq.lend(time + Duration::seconds(14)), Some(9));
+        assert_eq!(pq.top(), Some(as_key(0)));
+        pq.lend(time + Duration::seconds(5)); // 2 3 4 5 (1 6) 7 8 9 | <0/5>
+        assert_eq!(pq.top(), Some(as_key(2))); 
+        pq.lend(time + Duration::seconds(6)); // 3 4 5 (1 6) 7 8 9 | <0/10> <2/6>
+        assert_eq!(pq.top(), Some(as_key(3)));
+        pq.lend(time + Duration::seconds(7)); // 4 5 (1 6) 7 8 9 | <0/10> <2/6> <3/7>
+        assert_eq!(pq.top(), Some(as_key(4)));
+        pq.lend(time + Duration::seconds(8)); // 5 (1 6) 7 8 9 | <0/10> <2/6> <3/7> <4/8>
+        assert_eq!(pq.top(), Some(as_key(5)));
+        pq.lend(time + Duration::seconds(9)); // (1 6) 7 8 9 | <0/10> <2/6> <3/7> <4/8> <5/9>
+        assert_eq!(pq.top(), Some(as_key(6)));
+        pq.lend(time + Duration::seconds(10)); // 1 7 8 9 | <0/10> <2/6> <3/7> <4/8> <5/9> <6/10>
+        assert_eq!(pq.top(), Some(as_key(1)));
+        pq.lend(time + Duration::seconds(11)); // 7 8 9 | <0/10> <2/6> <3/7> <4/8> <5/9> <6/10> <1/11>
+        assert_eq!(pq.top(), Some(as_key(7)));
+        pq.lend(time + Duration::seconds(12)); // 8 9 | <0/10> <2/6> <3/7> <4/8> <5/9> <6/10> <1/11> <7/12>
+        assert_eq!(pq.top(), Some(as_key(8)));
+        pq.lend(time + Duration::seconds(13)); // 9 | <0/10> <2/6> <3/7> <4/8> <5/9> <6/10> <1/11> <7/12> <8/13>
+        assert_eq!(pq.top(), Some(as_key(9)));
+        pq.lend(time + Duration::seconds(14)); // | <0/10> <2/6> <3/7> <4/8> <5/9> <6/10> <1/11> <7/12> <8/13> <9/13>
+        assert_eq!(pq.len(), 0);
     }
 
     #[test]
     fn double_lend() {
         let time = SteadyTime::now();
-        let mut pq = PQueue::new(2);
-        assert_eq!(pq.lend(time + Duration::seconds(10)), Some(0));
-        assert_eq!(pq.lend(time + Duration::seconds(15)), Some(1));
-        pq.repay(1, RepayStatus::Penalty);
-        assert_eq!(pq.lend(time + Duration::seconds(20)), Some(1));
+        let mut pq = make_pq(2); // 0 1 |
+        assert_eq!(pq.top(), Some(as_key(0)));
+        pq.lend(time + Duration::seconds(10)); // 1 | <0/10>
+        assert_eq!(pq.top(), Some(as_key(1)));
+        pq.lend(time + Duration::seconds(15)); // | <0/10> <1/15>
+        assert_eq!(pq.len(), 0);
+        pq.repay(as_key(1), RepayStatus::Penalty); // 1 | <0/10>
+        assert_eq!(pq.top(), Some(as_key(1)));
+        pq.lend(time + Duration::seconds(20)); // | <0/10> <1/20>
+        assert_eq!(pq.len(), 0);
         assert_eq!(pq.next_timeout(), Some(time + Duration::seconds(10)));
-        pq.repay_timed_out();
+        pq.repay_timed_out(); // 0 | <1/20>
         assert_eq!(pq.next_timeout(), Some(time + Duration::seconds(20)));
-    }
-
-    #[test]
-    fn add() {
-        let mut pq = PQueue::new(2);
-        assert_eq!(pq.len(), 2);
-        assert_eq!(pq.add(), 2);
-        assert_eq!(pq.len(), 3);
-        assert_eq!(pq.add(), 3);
-        assert_eq!(pq.len(), 4);
+        assert_eq!(pq.top(), Some(as_key(0)));
     }
 }
 
