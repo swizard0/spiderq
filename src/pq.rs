35ca00ca677b7e94e0aab3043d9f0f8315b6b745
Alexey Voznyuk <me@swizard.info>
2015-11-15 04:17:13 +0300

    use RepayStatus reference instead of value

--- src/pq.rs
+++ src/pq.rs
@@ -71,7 +71,7 @@ impl PQueue {
         self.queue.peek().map(|e| e.key.clone())
     }
 
-    pub fn lend(&mut self, trigger_at: SteadyTime)  {
+    pub fn lend(&mut self, trigger_at: SteadyTime) {
         if let Some(entry) = self.queue.pop() {
             self.lentq.push(LentEntry { trigger_at: trigger_at, key: entry.key.clone(), snapshot: self.serial, });
             self.lentm.insert(entry.key.clone(), (self.serial, entry));
@@ -96,25 +96,25 @@ impl PQueue {
     
     pub fn repay_timed_out(&mut self) {
         if let Some(LentEntry { key: k, .. }) = self.lentq.pop() {
-            self.repay(k, RepayStatus::Front)
+            self.repay(k, &RepayStatus::Front)
         }
     }
 
-    pub fn repay(&mut self, key: Key, status: RepayStatus) {
+    pub fn repay(&mut self, key: Key, status: &RepayStatus) {
         if let Some((_, mut entry)) = self.lentm.remove(&key) {
             let min_priority = if let Some(&PQueueEntry { priority: p, .. }) = self.queue.peek() { p } else { 0 };
             let region = self.serial + 1 - min_priority;
             let current_boost = match status {
-                RepayStatus::Penalty if entry.boost == 0 => 0,
-                RepayStatus::Penalty => { entry.boost -= 1; entry.boost },
-                RepayStatus::Reward if region >> (entry.boost + 1) == 0 => entry.boost,
-                RepayStatus::Reward => { entry.boost += 1; entry.boost },
-                RepayStatus::Front => 0,
-                RepayStatus::Drop => return,
+                &RepayStatus::Penalty if entry.boost == 0 => 0,
+                &RepayStatus::Penalty => { entry.boost -= 1; entry.boost },
+                &RepayStatus::Reward if region >> (entry.boost + 1) == 0 => entry.boost,
+                &RepayStatus::Reward => { entry.boost += 1; entry.boost },
+                &RepayStatus::Front => 0,
+                &RepayStatus::Drop => return,
             };
             self.serial += 1;
             entry.priority = match status {
-                RepayStatus::Front => 0,
+                &RepayStatus::Front => 0,
                 _ => self.serial - (region - (region >> current_boost)),
             };
             self.queue.push(entry)
